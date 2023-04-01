83c0e9c20bb511b20b21910ac0dc2b910c162037
Tao Jin <tao-j@outlook.com>
2023-04-01 13:37:18 -0400

    vdb:

--- vdb.py
+++ vdb.py
@@ -7,31 +7,32 @@ import scipy.optimize
 matplotlib.rcParams["text.usetex"] = True
 import matplotlib.pyplot as plt
 
-rng = np.random.default_rng(555)
+rng = np.random.default_rng(233)
 mu = scipy.special.expit
-dtype = np.float32
+dtype = np.float64
 
-T = 1_000
+T = 1000
 delta = 1.0 / T
-d = 4
-sigma_0 = 1.0 / 4
+d = 3
+sigma_0 = 1.0 / 1
 L = int(np.ceil(1.0 / 2 / sigma_0))
-lmbda = 10
+lmbda = 0.01
 kappa = 0.1
 L_mu = 0.25
 M_mu = 0.25
-K = 4
+K = 8
 
 theta_star = rng.random((d,), dtype=dtype)
 theta_star = theta_star / np.sqrt(theta_star @ theta_star)
 x_orig = rng.integers(0, 2**d, size=(K,), dtype=np.int32)
 cA = ((x_orig.reshape(-1, 1) & (2 ** np.arange(d))) != 0) * 2.0 - 1.0
-# cA *= 10
+cA += rng.random(cA.shape)
+cA = rng.random((K, d)) - 1
 
 
 class VALDB:
     def __init__(self) -> None:
-        self.theta = np.ones((L + 1, d), dtype=dtype)
+        self.theta = np.ones((L + 1, d))
         self.V = np.zeros((L + 1, d, d), dtype=dtype) + np.eye(d) * lmbda
         self.Vinv = (
             1.0 / lmbda * np.zeros((L + 1, d, d), dtype=dtype) + np.eye(d) / lmbda
@@ -42,64 +43,63 @@ class VALDB:
 
     def MLE(self, l: int) -> None:
         theta_0 = self.theta[l]
-        mu_x = lambda theta: mu(self.xy_diff[l] @ theta)
-        mu_p_x = (
-            lambda theta: np.exp(self.xy_diff[l] @ theta)
-            / (np.exp(self.xy_diff[l] @ theta) + 1) ** 2
-        )
         func = lambda theta: (
             kappa * lmbda * theta @ theta
-            - (
-                self.r[l] * np.log(mu_x(theta))
-                + (1 - self.r[l]) * np.log(1 - mu_x(theta))
+            + (
+                np.log(np.exp(self.xy_diff[l] @ theta) + 1)
+                - self.r[l].reshape(-1, 1) * self.xy_diff[l] @ theta
             ).sum(axis=0)
         )
         grad = lambda theta: (
             2 * kappa * lmbda * theta
-            - (
-                (
-                    mu_p_x(theta)
-                    * (self.r[l] - mu_x(theta))
-                    / (1 - mu_x(theta))
-                    / mu_x(theta)
-                ).reshape(-1, 1)
+            + (
+                (mu(self.xy_diff[l] @ theta) - self.r[l]).reshape(-1, 1)
                 * self.xy_diff[l]
-            ).sum(axis=0)
+            )
+            .sum(axis=0)
+            .flatten()
         )
-        # a = grad(theta_0)
-        res = scipy.optimize.minimize(
-            func,
-            theta_0,
-            jac=grad,
-            method="BFGS",
-            options={"disp": False, "gtol": 1e-06},
-        )
-        self.theta[l] = res.x
-        # print(grad(res.x))
+
+        # gradient descent
+        for _ in range(20):
+            theta_0 -= 0.00001 * grad(theta_0)
+        self.theta[l] = theta_0
+        # print(theta_0)
+
+        # optimizer
+        # res = scipy.optimize.minimize(
+        #     func,
+        #     theta_0,
+        #     jac=grad,
+        #     method="BFGS",
+        #     options={"disp": True, "gtol": 1e-06},
+        # )
+        # self.theta[l] = res.x
+        # print(res.x)
         return None
 
     def main(self) -> None:
-        pp = mu(cA @ theta_star)
-        x_star_idx = np.argmax(pp)
-        print(x_star_idx, "x_star")
+        u = mu(cA @ theta_star)
+        x_star_idx = np.argmax(u)
+
         g_xy_diff = cA.reshape(K, 1, d) - cA.reshape(1, K, d)
         g_xy_diff_outer = g_xy_diff.reshape(K, K, d, 1) @ g_xy_diff.reshape(K, K, 1, d)
         p = mu(g_xy_diff @ theta_star)
         # sanity check
-        for i in range(K):
-            for j in range(K):
-                assert np.all(g_xy_diff[i, j] == cA[i] - cA[j])
-                assert np.all(
-                    g_xy_diff_outer[i, j] == np.outer(cA[i] - cA[j], cA[i] - cA[j])
-                )
-                assert p[i, j] == mu(cA[i] @ theta_star - cA[j] @ theta_star)
+        # for i in range(K):
+        #     for j in range(K):
+        #         assert np.all(g_xy_diff[i, j] == cA[i] - cA[j])
+        #         assert np.all(
+        #             g_xy_diff_outer[i, j] == np.outer(cA[i] - cA[j], cA[i] - cA[j])
+        #         )
+        #         assert (p[i, j] - mu(cA[i] @ theta_star - cA[j] @ theta_star)) < 1e-10
         print(p)
         print(cA)
         print(theta_star)
 
-        var = np.ones((L + 1, K, K), dtype=dtype)
+        var = np.ones((L, K, K), dtype=dtype)
         cPsi = np.zeros((L + 1))
-        for l in range(L + 1):
+        for l in range(L):
             var[l] = np.sqrt(
                 (g_xy_diff.reshape(K, K, 1, d) @ self.Vinv[l])
                 @ g_xy_diff.reshape(K, K, d, 1)
@@ -107,12 +107,16 @@ class VALDB:
         D = np.ones((L, K), dtype=dtype)
         for t in range(1, 1 + T):
             Dt = D.sum(axis=0) == L
-            # assert np.any(Dt)
+            assert np.any(Dt)
 
             mask = Dt.reshape(-1, 1) * Dt.reshape(1, -1)
+            Lvar = var + 1
+            sel = mask * Lvar.min(axis=0)
             x_i, y_i = np.unravel_index(
-                np.argmax(mask * var[:L].min(axis=0), axis=None), (K, K)
+                np.argmax(1 * Lvar.min(axis=0), axis=None), (K, K)
             )
+            if (x_i, y_i) == (1, 15):
+                pass
             x_i = rng.integers(0, K)
             y_i = rng.integers(0, K)
             x = cA[x_i]
@@ -120,6 +124,10 @@ class VALDB:
             xy_diff = x - y
             r = rng.binomial(1, p[x_i, y_i])
             self.R[t] = (2 * cA[x_star_idx] - x - y) @ theta_star + self.R[t - 1]
+            self.V[l] += g_xy_diff_outer[x_i, y_i]
+            self.V[L] += g_xy_diff_outer[x_i, y_i]
+            self.Vinv[l] = np.linalg.inv(self.V[l])
+            self.Vinv[L] = np.linalg.inv(self.V[L])
 
             self.xy_diff[L] = np.vstack([self.xy_diff[L], xy_diff])
             self.r[L] = np.append(self.r[L], r)
@@ -133,35 +141,37 @@ class VALDB:
                     + np.sqrt(lmbda)
                 )
             ).reshape((1,))
-            b_t *= 0.1
-            hat_sigma_t = np.sqrt(p_hat * (1 - p_hat) + b_t + b_t**2)
+            b_t *= 0.05
+            bar_sigma_t = np.sqrt(p_hat * (1 - p_hat) + b_t + b_t**2)
 
-            l = np.ceil(np.log2(hat_sigma_t / sigma_0)).astype(np.int32)[0]
+            l = np.ceil(np.log2(bar_sigma_t / sigma_0)).astype(np.int32)[0]
             l = np.clip(l, 1, L) - 1
-            l = rng.integers(0, L)
+            # l = rng.integers(0, L)
             cPsi[l] += 1
             cPsi[L] += 1
-            # print(hat_sigma_t, l, b_t, x_i, y_i)
+            print(bar_sigma_t, l, b_t, x_i, y_i)
+            self.xy_diff[l] = np.vstack([self.xy_diff[l], xy_diff])
+            self.r[l] = np.append(self.r[l], r)
+            self.MLE(l)
 
-            self.V[l] += g_xy_diff_outer[x_i, y_i]
-            self.V[L] += g_xy_diff_outer[x_i, y_i]
-            self.Vinv[l] = np.linalg.inv(self.V[l])
-            self.Vinv[L] = np.linalg.inv(self.V[L])
             eta_tl = (
                 16
                 * (2**l)
                 * sigma_0
                 / kappa
                 * np.sqrt(
-                    d * np.log(1 + cPsi[l] / d / lmbda) * np.log(4 * cPsi[l] / delta)
+                    d
+                    * np.log(1 + cPsi[l] / d / lmbda)
+                    * np.log(4 * cPsi[l] ** 2 / delta)
                 )
             )
             +4 / kappa * np.log(4 * (cPsi[l] ** 2) / delta)
-            eta_tl *= 0.001
+            eta_tl *= 1
             var[l] = np.sqrt(
                 (g_xy_diff.reshape(K, K, 1, d) @ self.Vinv[l])
                 @ g_xy_diff.reshape(K, K, d, 1)
             ).reshape(K, K)
+            # sanity check
             # print(var[l])
             # for i in range(K):
             #     for j in range(K):
@@ -171,18 +181,20 @@ class VALDB:
             cond = ucb.reshape(K, K) >= 0
             D[l] = cond.sum(axis=1) == K
 
-            self.xy_diff[l] = np.vstack([self.xy_diff[l], xy_diff])
-            self.r[l] = np.append(self.r[l], r)
-            self.MLE(l)
             print(f"{t}", end="\r")
+
+        print(x_star_idx, "x_star")
+        return cPsi
 
 
 v = VALDB()
-v.main()
+Psi = v.main()
 np.set_printoptions(precision=6, suppress=True)
 print(v.theta)
 print("----------")
 print(theta_star)
+print("layer sample", Psi)
 print(np.linalg.norm(v.theta - theta_star, axis=1))
-# plt.plot(v.R[1:])
-# plt.show()
+
+plt.plot(v.R[1:])
+plt.show()
