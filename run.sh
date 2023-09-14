672a8bf799937d2d998203b02d292a0562990539
Tao Jin <tao-j@outlook.com>
2023-09-14 19:46:54 -0400

    update slurm script

--- run.sh
+++ run.sh
@@ -1,7 +1,24 @@
 #!/bin/bash
-for i in {0..31}
-do
-	taskset --cpu-list $i python3 vdb.py $i &
-done
-wait
-python3 plot.py
+#SBATCH --job-name="test"
+#SBATCH --ntasks 1            # -n
+#SBATCH --cpus-per-task=1     # -c
+#SBATCH --ntasks-per-node=1   # 
+#SBATCH --nodes=1             # -N
+#SBATCH -t 00:10:00           # Wall time (hh:mm:ss)
+#SBATCH --error="my_job.err"
+#SBATCH --output="my_job.output"
+#SBATCH --partition=main
+## SBATCH --nodelist=hydro
+
+
+source /etc/profile.d/modules.sh
+# module load anaconda
+# module load python3 pmix-4.2.2 openmpi-4.0.2 cuda-toolkit
+# . ./venv/bin/activate
+
+source ~/.bashrc
+mamba activate nrm
+# Execute the MPI program
+# srun --mpi=pmix python3 mapreduce.py
+# taskset --cpu-list $1 \
+ python3 main.py
